39d89fc2f4b8effac315d8ec6b09a306161aabcb
Danil Shakalov <danilshakalov07@gmail.com>
2026-09-18 00:57:06 +0700

    feat: добавляем функции варианта 30 (Шакалов)

---
+++ shakalov.h
@@ -0,0 +1,11 @@
+#ifndef SHAKALOV_H
+#define SHAKALOV_H
+
+// Функции варианта 30 (Шакалов Д.)
+double fallSpeed(double t);
+double fallHeight(double t);
+double fallTime(double h);
+
+#endif // SHAKALOV_H
+
+
